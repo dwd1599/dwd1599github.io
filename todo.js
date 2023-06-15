3fe91c76b225401d124a6048f2879a9afa8eb797
dwd1599 <135197789+dwd1599@users.noreply.github.com>
2023-06-15 17:17:01 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -19,7 +19,7 @@ const colors = [
     '#668D3C',
     '#CAB0F1',
 ];
-let dragTarget = null;
+let dragTarget = {};
 
 function 색추출기(colors) {
     const 랜덤값 = Math.floor(Math.random() * colors.length); // 0 ~ 17 //18을 대체해주기 위한 텍스트 = colors.length(colors.갯수)
@@ -27,35 +27,44 @@ function 색추출기(colors) {
     return colors[랜덤값];
 }
 
-document.querySelector('button').addEventListener('click', (e) => {
+document.querySelector('button').addEventListener('click', e => {
     // (새로운태그) P태그 속 글씨 넣어주기
-    const text = document.querySelector('input').value;
+    const title = document.querySelector('.title').value;
+    const text = document.querySelector('.text').value;
+    const url = document.querySelector('.url').value;
+    const color = 색추출기(colors);
 
     //local storage 저장 (객체상태로 저장)
     // 1) text : 할일 텍스트, 사용자 인풋에 적은 내용.
     // 2) category : todo, doing, done
     // 3) id : 중복되지 않는 유니크한 값(현재시간)
     const todo객체 = {};
-    todo객체.text = document.querySelector('input').value;
+    todo객체.title = title;
+    todo객체.text = text;
+    todo객체.url = url;
+    todo객체.color = color;
+
     todo객체.category = 'todo';
     todo객체.id = Date.now();
     localStorage.setItem(todo객체.id, JSON.stringify(todo객체));
 
     //새로운 태그 생성
-    const newTag = createTag(text, todo객체.id);
+    const newTag = createTag(title, text, url, color, todo객체.id);
     document.querySelector('.todo').appendChild(newTag);
 
     //input 값 삭제 (초기화)
-    document.querySelector('input').value = '';
+    document.querySelector('.title').value = '';
+    document.querySelector('.text').value = '';
+    document.querySelector('.url').value = '';
 });
 
 const boxes = document.querySelectorAll('.box');
 console.log(boxes);
 boxes.forEach((box, i) => {
-    box.addEventListener('dragover', (e) => {
+    box.addEventListener('dragover', e => {
         e.preventDefault();
     });
-    box.addEventListener('drop', (e) => {
+    box.addEventListener('drop', e => {
         e.currentTarget.appendChild(dragTarget);
         const todo = JSON.parse(localStorage.getItem(dragTarget.getAttribute('key')));
         todo.category = e.currentTarget.getAttribute('category');
@@ -63,34 +72,71 @@ boxes.forEach((box, i) => {
     });
 });
 
-function createTag(text, key) {
-    // 새로운 P태그 요소를 생성
-    const newTag = document.createElement('p');
+function createTag(title, text, url, color, key) {
+    // 새로운 태그(newTag) 요소를 생성
+    const newTag = document.createElement('div');
+    newTag.style.backgroundColor = color;
+    newTag.classList.add('div');
 
-    // (새로운태그) 변수로 받아오기
-    newTag.innerHTML = text;
+    const tit = document.createElement('h4');
+    tit.textContent = title;
+    newTag.appendChild(tit);
+    tit.style.fontSize = '30px';
+    tit.style.fontWeight = '700';
 
-    // P 태그BOX의 랜덤 컬러 넣어주기
-    newTag.style.backgroundColor = 색추출기(colors);
+    const txt = document.createElement('p');
+    txt.textContent = text;
+    newTag.appendChild(txt);
+    txt.classList.add('txt');
+
+    //   const link = document.createElement("a");
+    //   link.href = url;
+    //   link.innerHTML = url;
+    //   newTag.appendChild(link);
+    //   link.classList.add("link");
+
+    const link = document.createElement('a');
+    link.href = url;
+    if (url.startsWith('http://') || url.startsWith('https://')) {
+        link.href = url;
+    } else {
+        // 기본값으로 설정할 링크
+        link.href = '';
+    }
+    link.target = '_blank'; // 새 탭에서 열기
+    //   link.addEventListener("click", (e) => {
+    //     e.preventDefault(); // 기본 동작을 막음
+    //     window.location.href = link.href; // 링크를 수동으로 처리하여 이동
+    //   });
+
+    link.innerHTML = url; // 링크 텍스트로 수정
+    newTag.appendChild(link);
+    link.classList.add('link');
+
+    const time = document.createElement('p');
+    time.innerHTML = new Date().toLocaleString();
+    newTag.appendChild(time);
+    time.classList.add('time');
+
     newTag.setAttribute('draggable', 'true');
 
     // P태그요소의 dragstart 이벤트 함수
-    newTag.addEventListener('dragstart', (e) => {
+    newTag.addEventListener('dragstart', e => {
         dragTarget = e.currentTarget;
     });
 
     /***** 삭제버튼 생성 코드 - 시작 *****/
     const deleteBtn = document.createElement('span');
     deleteBtn.classList.add('delete');
-    deleteBtn.innerHTML = 'X';
+    // deleteBtn.innerHTML = 'X';
+    deleteBtn.innerHTML = `<img src="delete.svg" alt="Delete">`;
 
     // 삭제버튼의 클릭 이벤트함수
-    deleteBtn.addEventListener('click', (e) => {
+    deleteBtn.addEventListener('click', e => {
         e.currentTarget.parentElement.remove();
         const key = e.currentTarget.parentElement.getAttribute('key');
         localStorage.removeItem(key);
     });
-    deleteBtn.style.float = 'right';
     newTag.appendChild(deleteBtn);
     /***** 삭제버튼 생성 코드 - 끝 *****/
 
@@ -104,7 +150,7 @@ function display() {
         const key = localStorage.key(i);
         const todo = JSON.parse(localStorage.getItem(key));
         const color = 색추출기(colors);
-        const newTag = createTag(todo.text, todo.id);
+        const newTag = createTag(todo.title, todo.text, todo.url, todo.color, todo.id);
         document.querySelector(`.${todo.category}`).appendChild(newTag);
     }
 }
